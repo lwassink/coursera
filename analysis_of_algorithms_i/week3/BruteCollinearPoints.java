0cd234bc4d8ab7e1b7dcbbccc0abe04da96dd6f7
Luke Wassink <lwassink@gmail.com>
2017-01-02 09:46:37 -0800

    work on week 3

---
+++ analysis_of_algorithms_i/week3/BruteCollinearPoints.java
@@ -0,0 +1,37 @@
+public class BruteCollinearPoints {
+  private LineSegment[] segments;
+  private int lineCount;
+
+  public BruteCollinearPoints(Point[] points) {
+    int length = points.length;
+    LineSegment[] tempSegments = new LineSegment[length];
+    lineCount = 0;
+
+    for (int i = 0; i < length - 3; i++) {
+      for (int j = i + 1; j < length - 2; j++) {
+        for (int k = j + 1; k < length - 1; k++) {
+          for (int l = k + 1; l < length; l++) {
+            double ij = points[i].slopeTo(points[j]);
+            double ik = points[i].slopeTo(points[k]);
+            double il = points[i].slopeTo(points[l]);
+
+            if (ij == ik && ik == il)
+              tempSegments[lineCount++] = new LineSegment(points[i], points[j]);
+          }
+        }
+      }
+    }
+
+    segments = new LineSegment[lineCount];
+    for (int i = 0; i < lineCount; i++)
+      segments[i] = tempSegments[i];
+  }
+
+  public int numberOfSegments() {
+    return lineCount;
+  }
+
+  public LineSegment[] segments() {
+    return segments;
+  }
+}
---
+++ analysis_of_algorithms_i/week3/FastCollinearPoints.java
@@ -0,0 +1,15 @@
+public class FastCollinearPoints {
+  private LineSegment[] segments;
+
+  public FastCollinearPoints(Point[] points) {
+    
+  }
+
+  public int numberOfSegments() {
+  }
+
+  public LineSegment[] segments() {
+    return segments;
+  }
+}
+
